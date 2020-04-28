8fe36b72200cedd6224dbf887f61df22b52c5db4
Mewen Le Roux <mewen.leroux@consulting-for.edenred.com>
2020-04-28 20:22:53 +0200

    clean html

--- webpack/plugins/html.js
+++ webpack/plugins/html.js
@@ -2,10 +2,8 @@ const path = require('path')
 const env = require('../helpers/environments')
 
 module.exports = {
-  template: path.resolve(__dirname, '../..', 'public/index.html'),
-  filename: env.prod
-    ? path.resolve(__dirname, '../..', 'server/index.html')
-    : /* default */ 'index.html',
-  customBody: env.prod ? '<!--vue-ssr-outlet-->' : '<div id="app"></div>',
   inject: /* don't reinject assets in prod */ env.dev,
+  customBody: env.prod ? '<!--vue-ssr-outlet-->' : '<div id="app"></div>',
+  template: path.resolve(__dirname, '../..', 'public/index.template.html'),
+  filename: env.prod ? path.resolve(__dirname, '../..', 'server/index.html') : 'index.html',
 }
